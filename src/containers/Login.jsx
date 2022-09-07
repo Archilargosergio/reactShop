39faf98e93a9b54dff14485b55e4ffb30c8e2226
Sergio Archila <archilargosergio@gmail.com>
2022-09-06 22:56:01 -0500

    Modified Logo accurate

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import '../styles/Login.scss'
+import React from 'react';
+import '../styles/Login.scss';
 
 const Login = () => {
     return (
  <div className="login-container">
    <div className="form-container">
-    <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+    <img src='/logos/logo_yard_sale.svg' alt="logo" className="logo" />
 
     <h1 className="title">Create a new parsword</h1>
 
