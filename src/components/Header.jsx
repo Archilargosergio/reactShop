75fe9d4a865e1de6e2b84d26045e3c3084aef8e3
Sergio Archila <archilargosergio@gmail.com>
2022-09-08 22:15:14 -0500

    Added Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../styles/Header.scss';
+
+const Header = () => {
+  return (
+    <nav>
+    <img src='/icons/icon_menu.svg' alt="menu" class="menu" />
+    <div className="navbar-left">
+        <img src='/logos/logo_yard_sale.svg' alt="logo" className="logo" />
+
+        <ul>
+          <li><a href="/">ALL</a></li>  
+          <li><a href="/">Clothles</a></li>  
+          <li><a href="/">Electronics</a></li>  
+          <li><a href="/">Furniture</a></li>  
+          <li><a href="/">Toys</a></li>  
+          <li><a href="/">Others</a></li>  
+        </ul>
+
+    </div>
+    <div className="navbar-right">
+       
+        <ul> 
+         <li className="navbar-email">platzi@example.com</li>
+         <li className="navbar-shopping-cart"><img src="./icons/icon_shopping_cart.svg" alt="shipping-cart" />
+          <div>2</div>
+         </li>
+        </ul>
+
+    </div>
+</nav>
+  )
+}
+
+export default Header;
