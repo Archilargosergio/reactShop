da822b8c92c433d68573f361652f3955da9683a5
Sergio Archila <archilargosergio@gmail.com>
2022-09-13 23:02:38 -0500

    Added useEffect

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,22 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ProductItem from '@components/ProductItem.jsx';
+import useGetProducts from '@hooks/useGetProducts.js';
 import '@styles/ProductList.scss';
-import axios from 'axios';
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
 
-	useEffect (async() => {
-	  const response = await axios(API);
-	  setProducts(response.data);
-	}, []); 
+  const products = useGetProducts(API);
+
   return (
     <section className="main-container">
 			<div className="ProductList">
 				{products.map(product => (
-					<ProductItem />
+					<ProductItem product={product} key={product.id} />
 				))}
 			</div>
 		</section>
